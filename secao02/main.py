45a74e4aae23ed946f4c1e66d358b5e95531366a
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-09-02 12:45:15 +0200

    instalando gunicorn

--- secao02/main.py
+++ secao02/main.py
@@ -4,8 +4,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-async def raiz():
-    return {"msg": "FastAPI na Geek University"}
+@app.get('/msg')
+async def mensagem():
+    return {"msg": "Test"}
 
 
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
